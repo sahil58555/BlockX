b7974271ff3977d7a55e51710dd23907ded9dbec
sahil58555 <sahil.saxena.58555@gmail.com>
2024-08-08 23:18:16 +0530

    changed backend url

--- Front-End/src/domain.js
+++ Front-End/src/domain.js
@@ -1,5 +1,5 @@
 // const domain = "http://localhost:5000";
-const domain = "https://blockpay-x05c.onrender.com";
+const domain = "https://blockx-bac.onrender.com";
 
 export const anonAadhaarDomain = "https://anon-aadhaar-chi.vercel.app";
 export default domain;
