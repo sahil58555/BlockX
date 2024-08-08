1ba9c14369e50ec0f2c108210e87a951c9f9a397
sahil58555 <sahil.saxena.58555@gmail.com>
2024-08-09 00:09:43 +0530

    changed anon aadhaar url

--- Front-End/src/domain.js
+++ Front-End/src/domain.js
@@ -1,5 +1,5 @@
 // const domain = "http://localhost:5000";
 const domain = "https://blockx-bac.onrender.com";
 
-export const anonAadhaarDomain = "https://anon-aadhaar-chi.vercel.app";
+export const anonAadhaarDomain = "https://anon-aadhaar-x.vercel.app/";
 export default domain;
